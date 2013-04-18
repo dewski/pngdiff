115c34c764c3fc4b1da359051ddedb8e83eb8deb
dewski <me@garrettbjerkhoel.com>
2013-04-17 21:23:32 -0700

    Make sure to set the response header to application/json

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/process", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+
 		values := r.URL.Query()
 		basePath := values["base"][0]
 		comparePath := values["compare"][0]
 		additions, deletions, diffs, err := pngdiff.Diff(basePath, comparePath)
 
 		if err == nil {
-			fmt.Fprintf(w, "{\"additions\": %d, \"deletions\": %d, \"diffs\": %d}", additions, deletions, diffs)
+			fmt.Fprintf(rw, "{\"additions\": %d, \"deletions\": %d, \"diffs\": %d}", additions, deletions, diffs)
 		}
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
