5ab66960a1c8bd1dab4514cdc8d62cd0df283342
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2019-01-15 23:15:35 -0500

    Remove ruby benchmark

--- benchmarks/ruby.rb
+++
@@ -1,9 +0,0 @@
-require 'chunky_png'
-
-base_path = File.expand_path('../../fixtures/large/base.png', __FILE__)
-target_path = File.expand_path('../../fixtures/large/target.png', __FILE__)
-base = ChunkyPNG::Image.from_file(base_path)
-target = ChunkyPNG::Image.from_file(target_path)
-
-puts base.area
-puts target.area
